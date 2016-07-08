251ef857e55cdeade84213f11f5ae9682df70fde
meanprogrammer <mean.programmer.d@gmail.com>
2016-07-08 14:08:10 +0800

    1234

---
+++ CRMAPi/Controllers/CRMController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KuyaTest.Controllers
+{
+    public class CRMController : ApiController
+    {
+        // GET api/crm
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/crm/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/crm
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/crm/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/crm/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
